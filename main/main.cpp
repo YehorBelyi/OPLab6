4440be2ae6ab273a00349ddcb63e3815cb7a2dd5
Yehor <egorwhite06@gmail.com>
2023-10-19 19:41:44 +0300

    Changed code a little bit to check it on github

--- main/main.cpp
+++ main/main.cpp
@@ -8,11 +8,20 @@ int main()
     // a = початкове значення x, b = кінцеве значення x, h = крок x
     double a = 0.5, b = 3.5, x, y, h = 0.5;
     cout << "Our range is [" << a << "; " << b << "] with step of " << h << endl;
-    x = a;
+   
+    /*x = a;
 
+    // Код до зміни
     do {
         y = sin(x) + fabs(x) + pow(2, x);
         cout << "If x = " << x << ", then y = " << y << endl;
         x += h;
     } while (x <= b);
+    */
+    
+    // Код після зміни для перевірки на Github
+    for (x = a; x <= b; x += h) {
+        y = sin(x) + fabs(x) + pow(2, x);
+        cout << "If x = " << x << ", then y = " << y << endl;
+    }
 }
