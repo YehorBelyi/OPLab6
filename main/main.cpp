0371dc624bd220dd935eeb7f1101f5b98cf957c4
Yehor <Yehor@DESKTOP-NHIEI3S>
2023-10-19 19:10:46 +0300

    Add project files.

---
+++ main/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iomanip>
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // a = початкове значення x, b = кінцеве значення x, h = крок x
+    double a = 0.5, b = 3.5, x, y, h = 0.5;
+    cout << "Our range is [" << a << "; " << b << "] with step of " << h << endl;
+    x = a;
+
+    do {
+        y = sin(x) + fabs(x) + pow(2, x);
+        cout << "If x = " << x << ", then y = " << y << endl;
+        x += h;
+    } while (x <= b);
+}
